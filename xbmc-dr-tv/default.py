676469cfb8901e225c5a235bda712495c1947593
Thomas Salomon <salomonhippie@gmail.com>
2011-02-28 15:06:37 +0100

    commented out unused code.

--- xbmc-dr-tv/default.py
+++ xbmc-dr-tv/default.py
@@ -6,17 +6,9 @@ from urllib2 import Request, urlopen, unquote
 
 baseURL = "http://www.dr.dk/NU/api/"
 allURL  = "programseries"
-epsInfoUrl = "default.aspx?template=programserie&guid=" #concat with prog GUID
-epsLinkUrl = "default.aspx?template=flad_programserie&guid=" #concat with prog GUID
 
 imgWidth = '415'
 imgHeight = '233'
-
-def unescape(s):
-        s = s.replace("&lt;", "<")
-        s = s.replace("&gt;", ">")
-        s = s.replace("&amp;", "&")
-        return s
 
 class DRtv:
 	
@@ -50,7 +42,7 @@ class DRtv:
 		return ((j['links'])[0])['uri']
 	
 	def endDirs(self):
-		xbmcplugin.endOfDirectory(self.handle, True, False, True)
+		xbmcplugin.endOfDirectory(self.handle, True, False, False)
 	
 	def epToItem(self, ep):
 		# print ep
@@ -66,24 +58,25 @@ class DRtv:
 			playpath = 'mp4:' + parts[1]
 			
 
-		print 'PUB: ' + ep['publishTime']
+		# print 'PUB: ' + ep['publishTime']
 			
 		li = xbmcgui.ListItem(label = ep['title'] + ' - ' + ep['formattedBroadcastTime'])
-
 		li.setProperty('PlayPath', playpath)
-
 		li.setInfo('video', {'title': ep['title'], "plot": ep['description'],
 				     'duration': ep['duration']})
 		img = baseURL + 'videos/' + str(ep['id']) + '/images/' + imgWidth + 'x' + imgHeight + '.jpg'
 		print 'IMG: ' + img
 		li.setThumbnailImage(img)
+		xbmcplugin.addDirectoryItem(self.handle, url, li, False, self.count)
 		return (url, li, False)
 
 	def listEpisodes(self):
 		showUrl = self.arg[6:]
 		eps = self.getEpisodes(showUrl)
+		self.count = len(eps)
 		items = map(self.epToItem, eps);
-		xbmcplugin.addDirectoryItems(self.handle, items, len(items))
+		xbmcplugin.setContent(self.handle, 'episodes')
+		# xbmcplugin.addDirectoryItems(self.handle, items, len(items))
 		self.endDirs()
 		
 	def progToDir(self, prog):
@@ -96,22 +89,24 @@ class DRtv:
 	def listProgs(self):
 		progs = self.getProgrammes(baseURL+allURL)		
 		dirs = map(self.progToDir, progs)
+		xbmcplugin.setContent(self.handle, 'tvshows')
 		xbmcplugin.addDirectoryItems(self.handle, dirs, len(dirs))
 		self.endDirs()
-	def playEpisode(self):
-		# this method is not used right now
-		epsUrl = self.arg[5:]
-		vurl = self.getVideoURL(Request(epsUrl))
-		parts = vurl.split('mp4:')
 
-		li = xbmcgui.ListItem(vurl)
-		li.setProperty('PlayPath', 'mp4:' + parts[1])
-		xbmcplugin.setResolvedUrl(self.handle, False, li)
-		#xbmcplugin.addDirectoryItem(self.handle, parts[0], li, False)
-		xbmc.Player(xbmc.PLAYER_CORE_DVDPLAYER).play(parts[0], li)
-		# dirs = [(parts[0], li, False)]
-		# xbmcplugin.addDirectoryItems(self.handle, dirs, len(dirs))
-		#self.endDirs()
+	# def playEpisode(self):
+	        # # this method is not used right now
+		# epsUrl = self.arg[5:]
+		# vurl = self.getVideoURL(Request(epsUrl))
+		# parts = vurl.split('mp4:')
+
+		# li = xbmcgui.ListItem(vurl)
+		# li.setProperty('PlayPath', 'mp4:' + parts[1])
+		# xbmcplugin.setResolvedUrl(self.handle, False, li)
+		# #xbmcplugin.addDirectoryItem(self.handle, parts[0], li, False)
+		# xbmc.Player(xbmc.PLAYER_CORE_DVDPLAYER).play(parts[0], li)
+		# # dirs = [(parts[0], li, False)]
+		# # xbmcplugin.addDirectoryItems(self.handle, dirs, len(dirs))
+		# #self.endDirs()
 
 if (__name__ == "__main__"):
 	handle = int(sys.argv[1])
